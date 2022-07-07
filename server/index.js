571fb5a059202525a87d9939bd2859e918e292a5
Juhyun Suh <shipalnomoo@gmail.com>
2022-07-07 18:57:51 -0500

    Setup packages

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/public')));
+
+// console.log(process.env.SECRET_KEY);
+
+// to API
+app.use('/*', (req, res) => {
+  res.send(599);
+});
+
+// app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
+});
