6a18f9cb6169cfddc55b0b361a0f3638d5f38ce3
Juhyun Suh <shipalnomoo@gmail.com>
2022-07-07 21:55:50 -0500

    Use sendStatus

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '/client/public')));
 
 // to API
 app.use('/*', (req, res) => {
-  res.send(599);
+  res.sendStatus(599);
 });
 
 // app.listen(process.env.PORT);
