1026af5d735cd6ab312f1b61632af272cebf9a1c
Max McKenna <mckennamax34@gmail.com>
2022-07-11 14:29:42 -0400

    Fixing my qna branch

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import Overview from './Overview/Overview';
 import RelatedItems from './RelatedItems/RelatedItems';
-import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -13,7 +12,6 @@ const App = function WhateverStupidName() {
       <h1>Hello World!!</h1>
       <Overview />
       <RelatedItems />
-      <QuestionsAndAnswers />
     </div>
   );
 };
