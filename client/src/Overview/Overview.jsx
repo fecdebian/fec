332c6273c55c615b95c6fc0174283f39b823057a
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-11 14:24:51 -0500

    partially added stars get request, still working on atom state

--- client/src/Overview/Overview.jsx
+++ client/src/Overview/Overview.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Stars from '../SharedComponents/ReviewStars';
 
 function Overview() {
   return (
     <div>
       Overview Placeholder
+      {Stars()}
     </div>
   );
 }
--- client/src/SharedComponents/ReviewStars.jsx
+++ client/src/SharedComponents/ReviewStars.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Stars(avgReview) {
-  // Placeholder until I write function
+  const [avgStars, setAvgStars] = useState(0);
+
+  useEffect(() => {
+    axios.get('/reviews/meta');
+    .then((reviews) => {
+    var ratingsObj = reviews.ratings;
+    var ratingsValues = Object.values(ratingsObj);
+    var sumOfRatings = 0;
+    var avgRating = 0
+    if (ratingsValues.length < 1) {
+
+    }
+    for (var i = 0; i < ratingsValues.length; i++) {
+      sumOfRatings += ratingsValues[i];
+    }
+    })
+    .catch((err) => console.log('Error getting average stars: ', err))
+  });
+
   return (
     <div>
       {avgReview}
