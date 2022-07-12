c8fc0dbb6749963cb14029ed8133dfac85790a20
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-11 19:07:13 -0500

    product detail page Promise.all stuff

--- client/src/ProductDetailPage.jsx
+++ client/src/ProductDetailPage.jsx
@@ -1,24 +1,48 @@
-import React, { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import axios from "axios";
 
-import Overview from './Overview/Overview';
-import RelatedItems from './RelatedItems/RelatedItems';
-import Reviews from './Reviews/Reviews';
-import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers';
-import currentProductState from './currentProduct';
+import Overview from "./Overview/Overview";
+import RelatedItems from "./RelatedItems/RelatedItems";
+import Reviews from "./Reviews/Reviews";
+import QuestionsAndAnswers from "./QuestionsAndAnswers/QuestionsAndAnswers";
+import currentProductState from "./currentProduct";
 
 // Huzzah for jsx!
 const ProductDetailPage = function WhateverStupidName() {
-  const [currentProduct, setCurrentProduct] = useRecoilState(currentProductState);
+  const [currentProduct, setCurrentProduct] =
+    useRecoilState(currentProductState);
+
+  let id;
 
   useEffect(() => {
-    axios.get('./products')
+    axios
+      .get("./products")
       .then((response) => {
         setCurrentProduct(response.data[0]);
+        id = response.data[0].id;
+      })
+      .then(() => {
+        console.log('currentProduct.id', id);
+        const getMetaReview = axios({
+          method: "get",
+          url: "/reviews/meta",
+          params: { product_id: id },
+        });
+        const getQuestions = axios({
+          method: "get",
+          url: "/qa/questions",
+          params: { product_id: id },
+        });
+        return Promise.all([getMetaReview, getQuestions]);
+      })
+      .then((responses) => {
+        responses.forEach((res) => {
+          console.log(res.data);
+        });
       })
       .catch((err) => {
-        console.error('Unable to get product from server ', err);
+        console.error("Unable to get product from server ", err);
       });
   }, []);
 
