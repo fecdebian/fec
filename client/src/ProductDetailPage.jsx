604f44689097a12dbdd12bf211b18827ec15f8ed
chenyou <chenyouhuang@gmail.com>
2022-07-12 14:26:59 -0400

    Decentralized the axios get request in ProductDetailPage file

--- client/src/ProductDetailPage.jsx
+++ client/src/ProductDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import axios from 'axios';
 
 import Overview from './Overview/Overview';
@@ -7,20 +7,12 @@ import RelatedItems from './RelatedItems/RelatedItems';
 import Reviews from './Reviews/Reviews';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers';
 import currentProductState from './currentProduct';
-<<<<<<< HEAD
-import relatedProductIDsState from './RelatedItems/ModelRelatedItems/relatedProductIDsState';
-=======
 import { questions as questionState } from './QuestionsAndAnswers/atoms';
->>>>>>> 03c111dfa7a3e07f979f0547241e2a4c4918d599
 
 // Huzzah for jsx!
 const ProductDetailPage = function WhateverStupidName() {
   const [currentProduct, setCurrentProduct] = useRecoilState(currentProductState);
-<<<<<<< HEAD
-  const setRelatedProductIDs = useSetRecoilState(relatedProductIDsState);
-=======
   const [questions, setQuestions] = useRecoilState(questionState);
->>>>>>> 03c111dfa7a3e07f979f0547241e2a4c4918d599
 
   let id;
 
@@ -43,22 +35,14 @@ const ProductDetailPage = function WhateverStupidName() {
           url: '/qa/questions',
           params: { product_id: id },
         });
-        const getRelatedProducts = axios({
-          method: 'get',
-          url: `/products?product_id=${id}/related`,
-          params: { product_id: id },
-        });
-        return Promise.all([getMetaReview, getQuestions, getRelatedProducts]);
+        return Promise.all([getMetaReview, getQuestions]);
       })
       .then((responses) => {
-<<<<<<< HEAD
         responses.forEach((res) => {
           console.log(res.data);
         });
-        setRelatedProductIDs(responses[2].data);
-=======
+        // setRelatedProductIDs(responses[2].data);
         setQuestions(responses[1].data.results);
->>>>>>> 03c111dfa7a3e07f979f0547241e2a4c4918d599
       })
       .catch((err) => {
         console.error('Unable to get product from server ', err);
--- client/src/RelatedItems/RelatedProductsCards/RelatedProductsCards.jsx
+++ client/src/RelatedItems/RelatedProductsCards/RelatedProductsCards.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useEffect } from 'react';
+import { useRecoilValue, useRecoilState } from 'recoil';
+import axios from 'axios';
+
 import ProductCard from './ProductCard';
 import relatedProductIDsState from '../ModelRelatedItems/relatedProductIDsState';
+import currentProductState from '../../currentProduct';
 
 export default function RelatedProductsCards() {
-  const reladtedProductIDs = useRecoilValue(relatedProductIDsState);
-  console.log(reladtedProductIDs);
+  const currentProduct = useRecoilValue(currentProductState);
+  const [relatedProductIDs, setRelatedProductIDs] = useRecoilState(relatedProductIDsState);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `/products?product_id=${currentProduct.id}/related`,
+      params: { product_id: currentProduct.id },
+    }).then((res) => {
+      setRelatedProductIDs(res.data);
+    })
+      .catch((err) => {
+        console.log('Unable to get related product id from server ', err);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log('related items are, ', relatedProductIDs);
+  }, [relatedProductIDs]);
 
   return (
     <div>
