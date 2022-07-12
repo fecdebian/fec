c527b07bf5c86afa06e1cb89029b49cfff375bd4
Max McKenna <mckennamax34@gmail.com>
2022-07-11 20:14:01 -0400

    Further work on qna module

--- client/src/QuestionsAndAnswers/QuestionsList.jsx
+++ client/src/QuestionsAndAnswers/QuestionsList.jsx
@@ -2,26 +2,27 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {
-  questions as questionsAtom, productID as productIDAtom, page as pageAtom, count as countAtom,
+  questions as questionsAtom, page as pageAtom, count as countAtom,
 } from './atoms';
 import Question from './Question';
 import currentProductState from '../currentProduct';
 
 function QuestionsList() {
   const [questions, setQuestions] = useRecoilState(questionsAtom);
-  const productID = useRecoilValue(productIDAtom);
+  const productID = useRecoilValue(currentProductState);
   const page = useRecoilValue(pageAtom);
   const count = useRecoilValue(countAtom);
 
   useEffect(() => {
-    axios.get(`/qa/questions?product_id=${productID}&page=${page}&count=${count}`)
+    axios.get(`/qa/questions?product_id=${productID.id}&page=${page}&count=${count}`)
       .then((res) => {
+        console.log('success');
         setQuestions(res.data.results);
       })
       .catch((err) => {
         console.log('error fetching questions:', err);
       });
-  }, [productID, page, count]);
+  }, [productID]);
   return (
     <ul>
       {questions.map((question) => <Question key={question.question_id} question={question} />)}
