073b1c7a4b1da0864eba308a2f153bb9c13cd6fc
Max McKenna <mckennamax34@gmail.com>
2022-07-12 16:55:55 -0400

    Added More Answered Questions button functionality

--- client/src/QuestionsAndAnswers/QuestionsList.jsx
+++ client/src/QuestionsAndAnswers/QuestionsList.jsx
@@ -2,19 +2,22 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {
-  questionsState, sortedQuestionsState, pageState, countState,
+  questionsState, sortedQuestionsState, countState, questionsViewState, moreQuestionsState
 } from './atoms';
 import currentProductState from '../currentProduct';
 import Question from './Question';
+import MoreAnsweredQuestions from './MoreAnsweredQuestions';
 
 function QuestionsList() {
-  const [questions, setQuestions] = useRecoilState(questionsState);
+  const [questions, setQuestions] = useRecoilState(questionsState); // setting up Recoil hooks
   const [sortedQuestions, setSortedQuestions] = useRecoilState(sortedQuestionsState);
+  const [questionsView, setQuestionsView] = useRecoilState(questionsViewState);
+  const [moreQuestions, setMoreQuestions] = useRecoilState(moreQuestionsState);
+
   const productID = useRecoilValue(currentProductState);
-  const page = useRecoilValue(pageState);
   const count = useRecoilValue(countState);
 
-  function quickSort(origArray) {
+  function quickSort(origArray) { // sorting function
     if (origArray.length <= 1) {
       return origArray;
     }
@@ -34,11 +37,8 @@ function QuestionsList() {
     return newArray.concat(quickSort(left), pivot, quickSort(right));
   }
 
-  useEffect(() => {
-    if (!productID.id) {
-      return;
-    }
-    axios.get(`/qa/questions?product_id=${productID.id}&page=${page}&count=${count}`)
+  useEffect(() => { // getting list of questions given product ID
+    axios.get(`/qa/questions?product_id=${productID.id}&page=${1}&count=${100}`)
       .then((res) => {
         console.log('successful GET questions request');
         setQuestions(res.data.results);
@@ -46,18 +46,31 @@ function QuestionsList() {
       .catch((err) => {
         console.log('error fetching questions:', err);
       });
-  }, [count, page, productID]);
+  }, [count, productID]);
 
-  useEffect(() => {
+  useEffect(() => { // sort list of questions by most to least helpful
     const copyQuestions = [...questions];
     setSortedQuestions(quickSort(copyQuestions));
   }, [questions]);
 
+  useEffect(() => {
+    const copySortedQuestions = [...sortedQuestions];
+    setMoreQuestions((copySortedQuestions.length - 2));
+    setQuestionsView(copySortedQuestions.slice(0, 2));
+  }, [sortedQuestions]);
+
+  if (questionsView.length === 0) {
+    return <h3>Loading...</h3>;
+  }
+
   return (
-    <ul>
-      {/* eslint-disable-next-line max-len */}
-      {sortedQuestions.map((question) => <Question key={question.question_id} question={question} />)}
-    </ul>
+    <div>
+      <ul>
+        {/* eslint-disable-next-line max-len */}
+        {questionsView.map((question) => <Question key={question.question_id} question={question} />)}
+      </ul>
+      <MoreAnsweredQuestions />
+    </div>
   );
 }
 export default QuestionsList;
