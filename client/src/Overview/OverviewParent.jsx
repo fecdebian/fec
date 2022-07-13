a69df8b41df9b1ff5ba68487b51b3600a885db89
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-13 13:45:20 -0500

    Correctly passing in the current style to present the price. Still need to work on styling of this though.

--- client/src/Overview/OverviewParent.jsx
+++ client/src/Overview/OverviewParent.jsx
@@ -7,14 +7,16 @@ import ReviewWeightedAverage from '../SharedComponents/ReviewWeightedAverage';
 import CatAndTitle from './CategoryAndTitle';
 import currentProductState from '../currentProduct';
 import StylePrice from './StylePrice';
+import { selectedProductStyle } from './overviewAtoms';
 
 function Overview() {
   const product = useRecoilValue(currentProductState);
+  const style = useRecoilValue(selectedProductStyle);
   return (
     <div>
       <ReviewWeightedAverage currentProduct={product} />
       <CatAndTitle />
-      <StylePrice currentProduct={product} />
+      <StylePrice currentProduct={product} currentStyle={style} />
     </div>
   );
 }
--- client/src/Overview/StylePrice.jsx
+++ client/src/Overview/StylePrice.jsx
@@ -8,13 +8,14 @@ import PropTypes from 'prop-types';
 
 import { currentProductStyles, selectedProductStyle } from './overviewAtoms';
 
-function StylePrice({ currentProduct }) {
+function StylePrice({ currentProduct, currentStyle }) {
   const product = currentProduct;
   const productID = product.id;
+  const style = currentStyle;
   const [productStyles, setProductStyles] = useRecoilState(currentProductStyles);
   const [selectedStyle, setSelectedStyle] = useRecoilState(selectedProductStyle);
   // Use effect for initial render and if productID changes
-  useEffect(() => {
+  function getStyles() {
     axios({
       method: 'get',
       url: `/products/${productID}/styles`,
@@ -22,44 +23,53 @@ function StylePrice({ currentProduct }) {
     })
       .then((response) => {
         const allStyles = {};
-        const defaultStyle = {};
+        let defaultStyle = {};
         response.data.results.forEach((result) => {
           allStyles[result.style_id] = result;
           if (result['default?'] === true) {
-            defaultStyle[result.style_id] = result;
+            defaultStyle = result;
           }
         });
         setProductStyles(allStyles);
         setSelectedStyle(defaultStyle);
       })
       .catch((err) => console.log('error getting product styles: ', err));
+  }
+
+  useEffect(() => {
+    getStyles();
   }, [product]);
+
   // Use effect for if selected style changes
-  if (selectedStyle.style_id === undefined) {
+  if (style.style_id === undefined) {
     return <h1>loading</h1>;
   }
-  console.log('selectedStyle: ', selectedStyle);
 
-  const orgPrice = selectedStyle.original_price;
-  const salePrice = selectedStyle.sale_price;
-  if (orgPrice === salePrice) {
+  if (style.sale_price === null) {
     return (
       <div>
-        {orgPrice}
+        $
+        {style.original_price}
       </div>
     );
   }
   return (
     <div>
-      {orgPrice}
-      {salePrice}
+      $
+      {style.original_price}
+      $
+      {style.sale_price}
     </div>
   );
 }
 
 StylePrice.propTypes = {
   currentProduct: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+  currentStyle: PropTypes.shape({
     style_id: PropTypes.number,
   }).isRequired,
 };
+
 export default StylePrice;
