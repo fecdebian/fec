5a66db077193ff5230f3bca716017990b9888e46
chenyou <chenyouhuang@gmail.com>
2022-07-18 12:13:32 -0400

    remove duplicate codes before refactor

--- client/src/RelatedItems/ModelRelatedItems/relatedProductsState.js
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-
-const relatedProductsState = atom({
-  key: 'relatedProductsState', // unique ID (with respect to other atoms/selectors)
-  default: [], // default value (aka initial value)
-});
-
-export default relatedProductsState;
