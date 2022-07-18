72e5b5a45d41f8a2ca058807b262b832d0d1a746
Juhyun Suh <shipalnomoo@gmail.com>
2022-07-18 15:39:20 -0500

    Fix typo on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,13 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    ['react', 'jest-dom'],
+    'react',
+    'jest-dom',
   ],
   rules: {
     "jest-dom/prefer-checked": "error",
     "jest-dom/prefer-enabled-disabled": "error",
     "jest-dom/prefer-required": "error",
     "jest-dom/prefer-to-have-attribute": "error",
-  },,
+  },
 };
