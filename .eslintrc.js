9110a5d3721c38c2f9d154935374acf2e1ed36a3
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-19 18:58:55 -0500

    fixed yarn.lock and eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:jest-dom/recommended',
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
@@ -17,7 +19,18 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest',
+    'jest-dom',
   ],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest-dom/prefer-checked': 'error',
+    'jest-dom/prefer-enabled-disabled': 'error',
+    'jest-dom/prefer-required': 'error',
+    'jest-dom/prefer-to-have-attribute': 'error',
   },
 };
