8e041fc01c38d5caa6fbaab29fb4d3acc150e29c
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-19 20:15:18 -0500

    added border highlighting to thumb once selected

--- client/src/Overview/GalleryThumbs.jsx
+++ client/src/Overview/GalleryThumbs.jsx
@@ -8,10 +8,30 @@ import { selectedImage } from './overviewAtoms';
 function ImageThumbnail({ image }) {
   const [currentImage, setCurrentImage] = useRecoilState(selectedImage);
 
+  if (image.url === currentImage) {
+    return (
+      <li>
+        <img
+          alt={image.thumbnail_url}
+          src={image.thumbnail_url} width="50"
+          height="50"
+          css={css`
+          position: relative;
+          border: solid 1px blue;
+          z-index: 1;
+          `}
+          onClick={() => {setCurrentImage(image.url)}}
+        />
+      </li>
+    );
+  }
+
   return (
     <li>
       <img
-        alt={image.thumbnail_url} src={image.thumbnail_url} width="50" height="50"
+        alt={image.thumbnail_url}
+        src={image.thumbnail_url} width="50"
+        height="50"
         css={css`
         position: relative;
         z-index: 1;
