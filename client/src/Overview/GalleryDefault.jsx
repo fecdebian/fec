1e645430fcc5d5264c560abe89b0f274b46dcd7d
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-07-20 11:18:04 -0500

    Basic arrow functionality, still need to get formatting close and then create scroll bars and modal

--- client/src/Overview/GalleryDefault.jsx
+++ client/src/Overview/GalleryDefault.jsx
@@ -1,22 +1,59 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useRecoilState } from 'recoil';
 
 import ImageThumbnail from './GalleryThumbs';
-import { selectedProductStyle, selectedImage } from './overviewAtoms';
+import { selectedProductStyle, selectedImage, selectedImageIndex } from './overviewAtoms';
 
 function GalleryDefault() {
   const currentStyle = useRecoilValue(selectedProductStyle);
-  const currentImage = useRecoilValue(selectedImage);
+  const [currentImage, setCurrentImage] = useRecoilState(selectedImage);
+  const [imageIndex, setImageIndex] = useRecoilState(selectedImageIndex);
   const imageList = currentStyle.photos;
 
   if (currentStyle.name === undefined) {
     return <div>Styles Loading...</div>;
   }
-  let keys = 0;
+
+  const previousPhotoHandler = (e) => {
+    e.preventDefault();
+    const photoIndex = imageIndex;
+    setCurrentImage(imageList[photoIndex - 1].url);
+    setImageIndex(photoIndex - 1);
+  };
+
+  const nextPhotoHandler = (e) => {
+    e.preventDefault();
+    const photoIndex = imageIndex;
+    setCurrentImage(imageList[photoIndex + 1].url);
+    setImageIndex(photoIndex + 1);
+  };
+
   return (
-    <div>
+    <div
+      css={css`
+          display:flex;
+          justify-content:left;
+          align-items: start;
+        `}
+    >
+      <i
+        className="fa-solid fa-arrow-left"
+        type="button"
+        onClick={previousPhotoHandler}
+        css={css`
+          z-index:5;
+          display:flex;
+          flex-direction: row;
+          justify-content: start;
+          color:black;
+          background-color:transparent;
+          border:none;
+          font-size:3rem;
+        `}
+      />
       <img
         alt={currentImage}
         src={currentImage}
@@ -24,12 +61,68 @@ function GalleryDefault() {
         height="500"
         css={css`
         position: absolute;
+        display: flex;
+        align-items: center;
         z-index: 0;
       `}
       />
+      <i
+        className="fa-solid fa-arrow-right"
+        type="button"
+        onClick={nextPhotoHandler}
+        css={css`
+          z-index:5;
+          display:flex;
+          flex-direction: row;
+          justify-content: end;
+          color:black;
+          background-color:transparent;
+          border:none;
+          font-size:3rem;
+      `}
+      />
 
-        {imageList.map((imageObj) =>
-          <ImageThumbnail image={imageObj} key={keys++} />)}
+      <div
+        css={css`
+          border-sizing: border-box;
+          display:flex;
+          flex-direction: column;
+          justify-content:left;
+          width:100;
+        `}
+      >
+        <i
+        className="fa-solid fa-arrow-up"
+        type="button"
+        onClick={previousPhotoHandler}
+        css={css`
+          z-index:5;
+          display:flex;
+          flex-direction: row;
+          justify-content: start;
+          color:black;
+          background-color:transparent;
+          border:none;
+          font-size:1rem;
+        `}
+      />
+        {imageList.map((imageObj, index) => <ImageThumbnail image={imageObj} key={index} imageIndex={index} />)}
+      </div>
+      <i
+        className="fa-solid fa-arrow-down"
+        type="button"
+        onClick={nextPhotoHandler}
+        css={css`
+          z-index:5;
+          display:flex;
+          flex-direction: row;
+          justify-content: end;
+          color:black;
+          background-color:transparent;
+          border:none;
+          font-size:1rem;
+      `}
+      />
 
     </div>
   );
